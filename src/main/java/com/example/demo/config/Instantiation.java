3050c3bbdcdf07d677e69f7654468419a880e717
VIANA <matheusgoldship@gmail.com>
2023-10-20 10:50:27 -0300

    Referenciando os POSTS com os usuarios!

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -39,16 +39,19 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		User matheus = new User(null, "Matheus","matheusgoldship@gmail.com" );
 		
 		
 		Post post1 = new Post(null, sdft.parse("21/03/2018"), "Partiu viajem", "vou para SP", new AuthorDto (maria));
 		Post post2 = new Post(null, sdft.parse("23/03/2018"), "Bom dia","Acordei feliz hoje",  new AuthorDto(maria));
 		Post post3 = new Post(null, sdft.parse("13/10/2023"), "Cheguei na area", "O pai viana ta na area e ataca novamente!", new AuthorDto());
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob, matheus));
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		 
-		postRepository.saveAll(Arrays.asList(post1, post2, post3));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		 maria.getPosts().addAll(Arrays.asList(post1, post2));
+		 userRepository.save(maria);
+		
 	}
 
 		 
